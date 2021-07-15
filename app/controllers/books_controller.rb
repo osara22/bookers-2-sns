4688bd8ca05f45fae7ff2bb8cb237b899171c9cf
osara22 <osaraiya22@gmail.com>
2021-07-15 11:12:54 +0000

    bookers-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,50 @@
 class BooksController < ApplicationController
   def new
-
+    @book = Book.new
   end
 
   def create
-
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to user_path(current_user.id)
+    else
+      render :show
+    end
   end
 
   def index
-
+    @user = current_user
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @books = Book.find(params[:id])
+    @book = Book.new
+  end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(@book.id)
   end
 
   def destroy
+    @books = Book.find(params[:id])
+    @books.destroy
+    redirect_to books_path
+  end
 
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '12f32728ea1ca7b2dc02e38fa683b2632ad3bf14cb405fcc8b5610c06c6d8270c3f4742c23891568eba0bbc297b4142507abcdf295cf059304ee0ac01a62a988'
