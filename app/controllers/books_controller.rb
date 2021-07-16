1efe9e9cab8fe1e6ac0f962fb46909a55b76f691
osara22 <osaraiya22@gmail.com>
2021-07-16 10:27:25 +0000

    [Finish]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to user_path(current_user.id)
+      redirect_to book_path(@book.id)
     else
       @user = current_user
       @books = Book.all
