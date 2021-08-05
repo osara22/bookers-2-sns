bf147326941095ff7a5d4e3d59476b27d8c37d98
osara22 <osaraiya22@gmail.com>
2021-08-05 12:02:31 +0000

    [Add]comment機能_async

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
-    
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    @books = Book.find(params[:book_id])
+    @book_comment = BookComment.new
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_back(fallback_location: root_path)
+    @books = Book.find(params[:book_id])
+    @book_comment = BookComment.new
   end
 
   private
